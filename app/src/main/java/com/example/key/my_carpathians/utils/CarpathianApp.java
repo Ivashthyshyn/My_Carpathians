ba741f45e9dfa13a40eddb291ace75c22671e525
key <Ivashthyshy@ukr.net>
2017-08-25 15:40:44 +0300

    fabric

--- app/src/main/java/com/example/key/my_carpathians/utils/CarpathianApp.java
+++ app/src/main/java/com/example/key/my_carpathians/utils/CarpathianApp.java
@@ -2,11 +2,8 @@ package com.example.key.my_carpathians.utils;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.ndk.CrashlyticsNdk;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.FirebaseDatabase;
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Key on 25.06.2017.
@@ -17,7 +14,6 @@ public class CarpathianApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         if (!FirebaseApp.getApps(this).isEmpty())
         {
             fb =FirebaseDatabase.getInstance();
